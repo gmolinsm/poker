90469efa448715ed9edecf6705787493f3510212
Guillermo <guimolins@hotmail.com>
2018-11-08 17:21:12 +0100

    Functions added

--- poker.py
+++ poker.py
@@ -17,6 +17,11 @@ class Card(object):
     def __str__(self):
         return "%s%s" % (self.rank, self.suit)
 
+    def __lt__(self, other):
+        if ranks.index(self.get_rank()) < ranks.index((other.get_rank())):
+            return True
+        return False
+
 
 class Deck(object):
     def __init__(self):
@@ -57,13 +62,115 @@ class Hand(object):
                     return True
         return False
 
+    def is_twopair(self):
+        ranks = []
+        for i in range(4):
+            if self.cards[i].get_rank() == self.cards[i+1].get_rank():
+                if not ranks.__contains__(self.cards[i].get_rank()):
+                    ranks.append(self.cards[i].get_rank())
+        if len(ranks) == 2:
+            if not self.is_fullhouse():
+                return True
+        return False
+
+    def is_threeofakind(self):
+        for i in range(5):
+            for j in range(i+1, 5):
+                for k in range(j+1, 5):
+                    if self.cards[i].get_rank() == self.cards[j].get_rank() \
+                            and self.cards[i].get_rank() == self.cards[k].get_rank():
+                        return True
+        return False
+
+    def is_straight(self):
+        self.cards.sort()
+        for i in range(4):
+            if ranks.index(self.cards[i].get_rank())+1 != ranks.index(self.cards[i+1].get_rank()):
+                return False
+        return True
+
+    def is_fullhouse(self):
+        count = 0
+        for i in range(4):
+            if self.cards[i].get_rank() != self.cards[i + 1].get_rank():
+                count += 1
+                if count > 1:
+                    return False
+        return True
+
+    def is_flush(self):
+        for i in range(4):
+            if self.cards[i].get_suit() != self.cards[i+1].get_suit():
+                return False
+        return True
+
+    def is_fourofakind(self):
+        for i in range(5):
+            for j in range(i+1, 5):
+                for k in range(j+1, 5):
+                    for l in range(k+1, 5):
+                        if self.cards[i].get_rank() == self.cards[j].get_rank() \
+                                and self.cards[i].get_rank() == self.cards[k].get_rank() \
+                                and self.cards[i].get_rank() == self.cards[l].get_rank():
+                                    return True
+        return False
+
+    def is_straightflush(self):
+        self.cards.sort()
+        for i in range(4):
+            if ranks.index(self.cards[i].get_rank()) + 1 != ranks.index(self.cards[i + 1].get_rank()):
+                return False
+            if self.cards[i].get_suit() != self.cards[i+1].get_suit():
+                return False
+        return True
+
+    def is_royalflush(self):
+        self.cards.sort()
+        if self.cards[0].get_rank() != 10:
+            return False
+        for i in range(4):
+            if ranks.index(self.cards[i].get_rank()) + 1 != ranks.index(self.cards[i + 1].get_rank()):
+                return False
+            if self.cards[i].get_suit() == self.cards[i+1].get_suit():
+                return False
+        return True
+
+    def is_highcard(self):
+        if self.is_pair():
+            return False
+        if self.is_twopair():
+            return False
+        if self.is_threeofakind():
+            return False
+        if self.is_flush():
+            return False
+        if self.is_fullhouse():
+            return False
+        if self.is_straight():
+            return False
+        if self.is_straightflush():
+            return False
+        if self.is_fourofakind():
+            return False
+        if self.is_royalflush():
+            return False
+        return True
 
 
+highcard = 0
+pair = 0
+twopair = 0
+threeofakind = 0
+flush = 0
+fullhouse = 0
+straight = 0
+straightflush = 0
+fourofakind = 0
+royalflush = 0
 
-new_deck = Deck()
-new_deck.shuffle()
-print(new_deck)
-hand = Hand(new_deck)
-print(hand)
-
-
+for i in range(10000):
+    new_deck = Deck()
+    new_deck.shuffle()
+    hand = Hand(new_deck)
+    if hand.is_threeofakind():
+        print(hand)
